8535e753449d3ab3a592a7929c40b42f8dd54c01
bianzCoSP <i_bianzjannkenrick.co@stratpoint.com>
2026-07-09 11:04:32 +0800

    chore(error): updated error testing page

--- app/errorTest/page.tsx
+++ app/errorTest/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
 export default function ErrorTest() {
-	throw new Error("Test Error: Thrown intentionally for Testing");
+	const [errorActive, setErrorActive] = useState(false);
+
+	if (errorActive) {
+		throw new Error("Test Error: Thrown intentionally for testing");
+	}
+
+	return (
+		<div className="flex flex-col flex-1 items-center justify-center">
+			<Button onClick={() => setErrorActive(true)}>Test Button</Button>
+		</div>
+	);
 }
